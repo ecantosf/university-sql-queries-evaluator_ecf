cda74ce1f6d640c9aa8e24afb5519c27ab8609c5
ecantosf <eduard.cantos@gmail.com>
2026-01-31 23:05:19 +0100

    feat: add query to retrieve youngest student data

--- queries/queries.sql
+++ queries/queries.sql
@@ -229,7 +229,11 @@ ORDER BY
     total DESC;
 
 -- 25. Retorna totes les dades de l'alumne/a més jove. (*)
-
+SELECT *
+FROM persona
+WHERE tipo = 'alumno'
+ORDER BY fecha_nacimiento DESC
+LIMIT 1;
 
 -- 26. Retorna un llistat amb els professors/es que tenen un departament associat i que no imparteixen cap assignatura. (apellido1, apellido2, nombre)
 
