3d11979712cf48a216e5e99426e89159527fc24c
ecantosf <eduard.cantos@gmail.com>
2026-01-31 21:16:17 +0100

    feat: list and sum by degree and subject type

--- queries/queries.sql
+++ queries/queries.sql
@@ -191,7 +191,13 @@ HAVING COUNT(a.id) > 40
 ORDER BY total DESC;
 
 -- 22. Retorna un llistat que mostri el nom dels graus i la suma del nombre total de crèdits que hi ha per a cada tipus d'assignatura. El resultat ha de tenir tres columnes: nom del grau, tipus d'assignatura i la suma dels crèdits de totes les assignatures que hi ha d'aquest tipus. (grau, tipus, total_creditos)
-
+SELECT
+	g.nombre AS grau,
+	a.tipo AS tipus,
+    SUM(a.creditos) AS total_creditos
+FROM grado g
+JOIN asignatura a ON a.id_grado = g.id
+GROUP BY g.nombre, a.tipo;
 
 -- 23. Retorna un llistat que mostri quants alumnes s'han matriculat d'alguna assignatura en cadascun dels cursos escolars. El resultat haurà de mostrar dues columnes, una columna amb l'any d'inici del curs escolar i una altra amb el nombre d'alumnes matriculats. (anyo_inicio, total)
 
