8cc7f673b6fae165069d85f316015c79943d4ec8
ecantosf <eduard.cantos@gmail.com>
2026-01-31 18:48:35 +0100

    Update queries.sql

--- queries/queries.sql
+++ queries/queries.sql
@@ -113,9 +113,9 @@ SELECT
     p.apellido1,
     p.apellido2,
     p.nombre
-FROM profesor pr
+FROM persona p
+LEFT JOIN profesor pr ON p.id = pr.id_profesor
 LEFT JOIN asignatura a ON pr.id_profesor = a.id_profesor
-RIGHT JOIN persona p ON pr.id_profesor = p.id
 WHERE p.tipo = 'profesor'
   AND a.id IS NULL;
 
