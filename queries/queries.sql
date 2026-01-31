43df9e3105badc0d8539025f7444c0ffc6fed039
ecantosf <eduard.cantos@gmail.com>
2026-01-31 19:55:37 +0100

    feat: add query to count professors per department with descending order

--- queries/queries.sql
+++ queries/queries.sql
@@ -152,7 +152,16 @@ WHERE tipo = 'alumno'
 	AND YEAR(fecha_nacimiento) = 1999;
 
 -- 18. Calcula quants professors/es hi ha en cada departament. El resultat només ha de mostrar dues columnes, una amb el nom del departament i una altra amb el nombre de professors/es que hi ha en aquest departament. El resultat només ha d'incloure els departaments que tenen professors/es associats i haurà d'estar ordenat de major a menor pel nombre de professors/es. (departamento, total)
-
+SELECT
+	d.nombre AS departamento,
+	COUNT(p.id_profesor) AS total
+FROM
+	departamento d
+    JOIN profesor p ON d.id = p.id_departamento
+GROUP BY
+	d.id, d.nombre
+ORDER BY
+	total DESC;
 
 -- 19. Retorna un llistat amb tots els departaments i el nombre de professors/es que hi ha en cadascun d'ells. Tingui en compte que poden existir departaments que no tenen professors/es associats. Aquests departaments també han d'aparèixer en el llistat. (departamento, total)
 
